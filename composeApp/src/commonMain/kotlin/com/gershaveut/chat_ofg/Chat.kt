ceada60ca31c7baa03fa25b9051b6f46e4b9adc8
Gershaveut <vova1234567890012011@yandex.ru>
2024-09-08 16:38:27 +0300

    Change of location

---
+++ composeApp/src/commonMain/kotlin/com/gershaveut/chat_ofg/Chat.kt
@@ -0,0 +1,194 @@
+package com.gershaveut.chat_ofg
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.Send
+import androidx.compose.material.icons.outlined.Build
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.gershaveut.chat_ofg.data.AbstractChat
+import com.gershaveut.chat_ofg.data.Message
+import com.gershaveut.chat_ofg.data.MessageStatus
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+@Composable
+fun ChatScreen(chat: AbstractChat) {
+	Column {
+		// Message
+		LazyColumn(modifier = Modifier.weight(15f)) {
+			var previousMessage: Message? = null
+			
+			items(chat.messages) { message ->
+				val chatBoxModifier =
+					Modifier.sizeIn(maxWidth = 350.dp).padding(top = 5.dp, start = 5.dp, end = 5.dp)
+				
+				if (previousMessage != null && message.sendTime.date != previousMessage!!.sendTime.date) {
+					val data = message.sendTime.date
+					
+					val dataText =
+						if (data.year == clientDataTime.year)
+							"${data.dayOfMonth} ${data.month.name}"
+						else
+							cdToString(data)
+					
+					Row(
+						horizontalArrangement = Arrangement.Center,
+						modifier = Modifier.fillMaxWidth()
+					) {
+						Box(
+							modifier = Modifier.padding(5.dp)
+								.background(
+									color = Color(0, 0, 0, 50),
+									shape = RoundedCornerShape(15.dp)
+								)
+						) {
+							Text(dataText, modifier = Modifier.padding(10.dp, 5.dp))
+						}
+					}
+				}
+				
+				previousMessage = message
+				
+				if (message.owner != clientUser) {
+					Box(
+						modifier = chatBoxModifier
+							.background(
+								color = Color(238, 238, 238),
+								shape = RoundedCornerShape(10.dp)
+							)
+					) {
+						messageContent(message)
+					}
+				} else {
+					Row(
+						modifier = Modifier.fillMaxWidth(),
+						horizontalArrangement = if (calculateWindowSizeClass().widthSizeClass == WindowWidthSizeClass.Compact) Arrangement.End else Arrangement.Start
+					) {
+						Box(
+							modifier = chatBoxModifier
+								.background(
+									color = Color(199, 225, 252),
+									shape = RoundedCornerShape(10.dp)
+								)
+						) {
+							messageContent(message)
+						}
+					}
+				}
+			}
+		}
+		
+		// Chat box
+		Row {
+			val message = remember { mutableStateOf("") }
+			
+			TextField(
+				message.value, { text ->
+					message.value = text
+				},
+				modifier = Modifier
+					.weight(1f)
+					.fillMaxWidth(),
+				placeholder = { Text("Print here...") }
+			)
+			IconButton(
+				{
+					chat.messages.add(
+						Message(
+							clientUser,
+							message.value,
+							Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()),
+							MessageStatus.UnSend
+						)
+					)
+				},
+				modifier = Modifier.size(50.dp)
+			) {
+				Icon(Icons.AutoMirrored.Outlined.Send, null)
+			}
+		}
+	}
+}
+
+@Composable
+fun messageContent(message: Message) {
+	Column(horizontalAlignment = Alignment.CenterHorizontally) {
+		Text(
+			message.owner.displayName,
+			color = Color(41, 150, 201),
+			fontSize = 15.sp,
+			modifier = Modifier
+				.padding(top = 10.dp, start = 10.dp, end = 10.dp)
+				.align(Alignment.Start)
+		)
+		
+		/*
+		if (message.id != null) {
+			Image(ImageBitmap.imageResource(message.id!!), null)
+		}
+		*/
+		
+		Text(
+			message.text, modifier = Modifier
+				.padding(
+					top = 10.dp,
+					start = 10.dp,
+					bottom = 5.dp,
+					end = 10.dp
+				).align(Alignment.Start)
+		)
+		
+		Row(
+			Modifier.align(Alignment.End).padding(
+				bottom = 10.dp,
+				end = 10.dp
+			)
+		) {
+			Text(
+				message.sendTime.time.toString(),
+				color = Color.Gray,
+				fontSize = 10.sp,
+				modifier = Modifier
+					.padding(end = 5.dp)
+			)
+			
+			MessageStatusIcon(message.messageStatus)
+		}
+	}
+}
+
+@Composable
+fun MessageStatusIcon(messageStatus: MessageStatus) {
+	when (messageStatus) {
+		MessageStatus.UnSend -> Icon(Icons.Outlined.Build, "UnSend")
+		MessageStatus.UnRead -> Text("!", color = Color.Gray, fontSize = 10.sp)
+		MessageStatus.Read -> Text("!!", color = Color.Magenta, fontSize = 10.sp)
+	}
+}
---
+++ composeApp/src/commonMain/kotlin/com/gershaveut/chat_ofg/Menu.kt
@@ -0,0 +1,183 @@
+package com.gershaveut.chat_ofg
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ExitToApp
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import com.gershaveut.chat_ofg.data.AbstractChat
+import com.gershaveut.chat_ofg.data.MessageStatus
+
+@Composable
+fun Menu() {
+	val openChat: MutableState<AbstractChat?> = remember { mutableStateOf(null) }
+	
+	Column {
+		if (openChat.value == null) {
+			// Menu
+			TopAppBar(
+				title = { Text("ChatOFG") },
+				navigationIcon = {
+					IconButton({
+					
+					}) { Icon(Icons.Filled.Menu, contentDescription = "Menu") }
+				},
+			)
+			
+			LazyColumn {
+				items(chats) { chat ->
+					Row(
+						modifier = Modifier.fillMaxWidth().padding(1.dp).clickable {
+							chat.messages.map {
+								if (it.owner != clientUser && it.messageStatus == MessageStatus.UnRead) it.messageStatus =
+									MessageStatus.Read
+							}
+							
+							openChat.value = chat
+						}
+					) {
+						// Image box
+						Box(
+							contentAlignment = Alignment.Center,
+							modifier = Modifier.background(
+								color = Color.LightGray,
+								shape = CircleShape
+							).size(45.dp)
+						) {
+							Text(chat.name.toCharArray()[0].toString().uppercase())
+						}
+						
+						val lastMessage = chat.messages.last()
+						
+						// Info
+						Column {
+							// Row Name and time
+							Row(
+								modifier = Modifier.fillMaxWidth(),
+								verticalAlignment = Alignment.CenterVertically,
+								horizontalArrangement = Arrangement.SpaceBetween
+							) {
+								Text(chat.name, textAlign = TextAlign.Start)
+								
+								Row {
+									if (lastMessage.owner == clientUser)
+										MessageStatusIcon(lastMessage.messageStatus)
+									
+									Text(
+										cdtToString(lastMessage.sendTime),
+										fontSize = 10.sp,
+										color = Color.Gray,
+										modifier = Modifier.padding(start = 5.dp)
+									)
+								}
+							}
+							
+							// Row Last Text and New Message
+							Row(
+								modifier = Modifier.fillMaxWidth(),
+								verticalAlignment = Alignment.CenterVertically,
+								horizontalArrangement = Arrangement.SpaceBetween
+							) {
+								Text(
+									lastMessage.text,
+									textAlign = TextAlign.Start,
+									fontSize = 12.sp,
+									color = Color.Gray
+								)
+								
+								val unread =
+									chat.messages.count { it.messageStatus == MessageStatus.UnRead }
+								
+								if (unread > 0) {
+									Text(
+										unread.toString(),
+										textAlign = TextAlign.Center,
+										modifier = Modifier.background(
+											color = Color.Cyan,
+											shape = CircleShape
+										).size(25.dp)
+									)
+								}
+							}
+						}
+					}
+				}
+			}
+		} else {
+			// Chat
+			val showInfo = remember { mutableStateOf(false) }
+			
+			TopAppBar(
+				title = {
+					Text(
+						openChat.value!!.name,
+						modifier = Modifier.clickable { showInfo.value = true })
+				},
+				navigationIcon = {
+					IconButton({
+						openChat.value = null
+					}) { Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back") }
+				},
+			)
+			
+			ChatScreen(openChat.value!!)
+			
+			if (showInfo.value) {
+				Dialog(onDismissRequest = {
+					showInfo.value = false
+				}) {
+					Surface(
+						modifier = Modifier.size(250.dp, 500.dp)
+					) {
+						Column {
+							TopAppBar(
+								title = {
+									Text("Info")
+								},
+								actions = {
+									IconButton({
+										showInfo.value = false
+									}) {
+										Icon(
+											Icons.AutoMirrored.Filled.ExitToApp,
+											contentDescription = "Close"
+										)
+									}
+								}
+							)
+							
+							openChat.value!!.ShowInfo()
+						}
+					}
+				}
+			}
+		}
+	}
+}
